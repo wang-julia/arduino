3a02f3bc994818f48aad154a26b8a595b43b1f24
Julia Wang <jwang.kq@gmail.com>
2025-10-22 15:35:11 -0400

    Add files via upload

--- hoopSensor.ino
+++ hoopSensor.ino
@@ -1,12 +1,47 @@
+
+const int trigPin = 12; //sends signal
+const int echoPin = 13; //reads duration
+const double threshold = 7; //7 cm threshold
+int score = 0; //keeps track of score
+
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-  //hello world
+  Serial.begin(9600); //speed of communication between arduino and computer
 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
 
+}
+
+double distance()
+{
+  double duration = 0; 
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  return (duration/2) / 29.1; //in cm
+}
+
+boolean detectShot()
+{
+  return distance() <= threshold; 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if (detectShot())
+  {
+    Serial.print("Basketball Detected");
+    score += 1;
+    delay(1000); // 1 second
+  }
+  
+
 }
+
